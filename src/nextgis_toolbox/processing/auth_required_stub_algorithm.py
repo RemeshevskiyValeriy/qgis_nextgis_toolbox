16c8d4d9d8cfe1f36baf7f4ad337540bba8b367d
Valeriy Remeshevskiy <remeshevskiy@gmail.com>
2026-05-13 12:59:27 +0300

    Add stub algorithm

---
+++ src/nextgis_toolbox/processing/auth_required_stub_algorithm.py
@@ -0,0 +1,45 @@
+# NextGIS Toolbox Plugin
+# Copyright (C) 2026  NextGIS
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or any
+# later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, see <https://www.gnu.org/licenses/>.
+
+from qgis.core import (
+    QgsProcessingAlgorithm,
+)
+
+from nextgis_toolbox.ui.icon import qgis_icon
+
+
+class AuthRequiredStubAlgorithm(QgsProcessingAlgorithm):
+    """
+    Stub algorithm shown when user is not authenticated.
+    """
+
+    def initAlgorithm(self, configuration=None):
+        pass
+
+    def name(self):
+        return "authentication_required"
+
+    def displayName(self):
+        return "Authentication required"
+
+    def icon(self):
+        return qgis_icon("mIconDelete.svg")
+
+    def processAlgorithm(self, parameters, context, feedback):
+        return {}
+
+    def createInstance(self):
+        return AuthRequiredStubAlgorithm()

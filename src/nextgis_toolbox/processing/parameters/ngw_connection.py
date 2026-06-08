28fd3eb9521f040f726afab7e2aadd45c6c0846f
Ivan Barsukov <i.barsukov@protonmail.com>
2026-06-08 08:24:42 +0300

    Add secrets detection in pre-commit

--- src/nextgis_toolbox/processing/parameters/ngw_connection.py
+++ src/nextgis_toolbox/processing/parameters/ngw_connection.py
@@ -41,7 +41,7 @@ if TYPE_CHECKING:
 
 NGW_URL_SUFFIX = "_url"
 NGW_LOGIN_SUFFIX = "_login"
-NGW_PASSWORD_SUFFIX = "_password"
+NGW_PASSWORD_SUFFIX = "_password"  # pragma: allowlist secret
 
 
 @dataclass(frozen=True)
